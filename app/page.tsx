f9ab31e509ab7a97cab90e43eb6c824987d8a5dd
solan <mariesolangemc@gmail.com>
2025-02-21 14:54:26 -0500

    fix: test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import {Button} from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-      <>
-          <h1> intercert cms</h1> <Button>Click me</Button>
-      </>
+
+      <main>
+        <h1> intercert</h1>
+      </main>
   );
 }
