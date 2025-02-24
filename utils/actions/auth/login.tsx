10bea04efbd09fb2f16be922b0b48ab1d5d350e1
neo <defneorise@gmail.com>
2025-02-24 10:19:51 -0500

    feat: add response to login supabase action

--- utils/actions/auth/login.tsx
+++ utils/actions/auth/login.tsx
@@ -1,26 +1,27 @@
 'use server'
 
 import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
 
-export async function login(formData: FormData) {
+
+
+export async function login(values: { email: string; password: string; }) {
     const supabase = await createClient()
 
     // type-casting here for convenience
     // in practice, you should validate your inputs
     const data = {
-        email: formData.get('email') as string,
-        password: formData.get('password') as string,
+        email: values.email as string,
+        password: values.password as string,
     }
 
     const { error } = await supabase.auth.signInWithPassword(data)
 
     if (error) {
-        redirect('/error')
+        return error.message;
     }
 
     revalidatePath('/', 'layout')
-    redirect('/account')
+    return "success";
 }
