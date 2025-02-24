2c814c7c3304bfabc73169016e600f45efc18f42
neo <defneorise@gmail.com>
2025-02-24 13:44:40 -0500

    fix: provisional redirect in home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import {redirect} from "next/navigation";
 
 export default function Home() {
 
+  // redirect to login page if not logged in else redirect to dashboard
+  // TODO: add authentication logic
   redirect("/login");
-  return (
-      <h1> intercert</h1>
-  );
 }
